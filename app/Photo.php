1bba13fd055ebfca906ce2c83a405bd2de77743a
yotaarai <yotaarai85@gmail.com>
2019-12-27 16:58:42 +0900

    feat: いいね機能API実装

--- app/Photo.php
+++ app/Photo.php
@@ -91,5 +91,12 @@ class Photo extends Model
         return Storage::cloud()->url($this->attributes['filename']);
     }
 
-    
+    /**
+     * リレーションシップ - usersテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function likes()
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
 }
